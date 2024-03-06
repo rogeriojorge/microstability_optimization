42592007bfd848fcaec543f5516abf9b6f61e684
Rogerio Jorge <rogeriodejesusjorge@gmail.com>
2024-03-06 09:57:17 -0600

    self consistent optimizations

--- src/finite_beta/main.py
+++ src/finite_beta/main.py
@@ -29,12 +29,12 @@ parser.add_argument("--type", type=int, default=1)
 args = parser.parse_args()
 
 QA_or_QH = 'QH' if args.type == 1 else 'QA'
-MAXITER = 15
+MAXITER = 150
 optimize_well = True
 optimize_DMerc = True
 optimize_shear = True
 plot_result = True
-max_modes = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5]
+max_modes = [2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5]
 
 beta = 2.5 #%
 diff_method = 'forward'
@@ -44,8 +44,8 @@ maxmodes_mpol_mapping = {1: 5, 2: 5, 3: 5, 4: 6, 5: 6}
 ftol = 1e-6
 xtol = 1e-6
 aspect_ratio = 6.5
-shear_min_QA = 0.15
-shear_min_QH = 0.20
+shear_min_QA = 0.10
+shear_min_QH = 0.10
 shear_weight = 1e-2
 iota_min_QA = 0.42
 iota_min_QH = 1.05

6b12713cfdda536cb240d03b2bbc80e451cb333a
Rogerio Jorge <rogeriodejesusjorge@gmail.com>
2024-03-20 10:58:54 -0500

    new sims

--- src/circular_coil_stellarator/main.py
+++ src/circular_coil_stellarator/main.py
@@ -38,9 +38,9 @@ max_mode_array = [1]*0 + [2]*0 + [3]*0 + [4]*0 + [5]*4 + [6]*4
 nmodes_coils = 2
 aspect_ratio_target = 5
 JACOBIAN_THRESHOLD = 30
-aspect_ratio_weight = 8e-2  # 2e-2 for nfp3
-iota_min_QA = 0.29 # 0.13 for nfp3
-iota_min_QH = 0.29 # 0.13 for nfp3
+aspect_ratio_weight = 4e-2  # 2e-2 for nfp3
+iota_min_QA = 0.17 # 0.17 for nfp3 # 0.29 for nfp4
+iota_min_QH = 0.17 # 0.17 for nfp3 # 0.29 for nfp4
 maxmodes_mpol_mapping = {1: 3, 2: 5, 3: 5, 4: 6, 5: 6, 6: 6}
 coils_objective_weight = 8e+2
 if args.type == 1: QA_or_QH = 'simple_nfp1'

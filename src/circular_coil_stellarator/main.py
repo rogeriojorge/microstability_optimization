c0f55264dcf7aa27f04cbad22df834df1505e57b
Rogerio Jorge <rogeriodejesusjorge@gmail.com>
2024-03-21 15:11:18 -0500

    nfp4 nonplanar

--- src/circular_coil_stellarator/main.py
+++ src/circular_coil_stellarator/main.py
@@ -38,21 +38,21 @@ else: raise ValueError('Invalid type')
 ############## Input parameters
 ##########################################################################################
 use_previous_coils = True
-optimize_stage_1_with_coils = False
+optimize_stage_1_with_coils = True
 planar_coils = False
 MAXITER_stage_1 = 10
 MAXITER_stage_2 = 250
 MAXITER_single_stage = 15
 MAXFEV_single_stage = 21
 LENGTH_THRESHOLD = 2.8
-max_mode_array = [1]*2 + [2]*4 + [3]*4 + [4]*4 + [5]*4 + [6]*4
+max_mode_array = [1]*0 + [2]*4 + [3]*4 + [4]*4 + [5]*4 + [6]*4
 # max_mode_array = [1]*0 + [2]*0 + [3]*0 + [4]*4 + [5]*4 + [6]*4
 nmodes_coils = 4
-aspect_ratio_target = 5
+aspect_ratio_target = 6
 JACOBIAN_THRESHOLD = 30
 aspect_ratio_weight = 8e-2 if QA_or_QH=='simple_nfp4' else (4e-2 if QA_or_QH=='simple_nfp3' else 5e-3)
-iota_min_QA = 0.343 if QA_or_QH=='simple_nfp4' else (0.175 if QA_or_QH=='simple_nfp3' else 0.11)
-iota_min_QH = 0.343 if QA_or_QH=='simple_nfp4' else (0.175 if QA_or_QH=='simple_nfp3' else 0.11)
+iota_min_QA = 0.337 if QA_or_QH=='simple_nfp4' else (0.175 if QA_or_QH=='simple_nfp3' else 0.11)
+iota_min_QH = 0.337 if QA_or_QH=='simple_nfp4' else (0.175 if QA_or_QH=='simple_nfp3' else 0.11)
 maxmodes_mpol_mapping = {1: 5, 2: 6, 3: 6, 4: 7, 5: 7, 6: 7}
 coils_objective_weight = 1e+3
 CC_THRESHOLD = 0.1

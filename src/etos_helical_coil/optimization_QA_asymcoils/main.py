67b3e0934d538f44bb48ac7bfb0223f36e2a9ab8
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2024-04-02 09:00:17 -0500

    same

--- src/etos_helical_coil/optimization_QA_asymcoils/main.py
+++ src/etos_helical_coil/optimization_QA_asymcoils/main.py
@@ -73,7 +73,7 @@ nfp_min_iota_nfp4 = 0.252; nfp_min_iota_nfp3 = 0.175; nfp_min_iota = 0.11; nfp_m
 iota_min_QA = nfp_min_iota_QA if QA_or_QH=='QA' else (nfp_min_iota_nfp4 if QA_or_QH=='simple_nfp4' else (nfp_min_iota_nfp3 if QA_or_QH=='simple_nfp3' else nfp_min_iota))
 iota_min_QH = nfp_min_iota_QH if QA_or_QH=='QH' else (nfp_min_iota_nfp4 if QA_or_QH=='simple_nfp4' else (nfp_min_iota_nfp3 if QA_or_QH=='simple_nfp3' else nfp_min_iota))
 maxmodes_mpol_mapping = {1: 5, 2: 5, 3: 5, 4: 6, 5: 6, 6: 7}
-coils_objective_weight = 3e+3 if 'QI' in QA_or_QH else 1e+4
+coils_objective_weight = 1e+3 if 'QI' in QA_or_QH else 1e+4
 CC_THRESHOLD = 0.03
 quasisymmetry_weight = 5e+0 # 1e-0 if 'QI' in QA_or_QH else 1e+2
 # QA_or_QH = 'simple' # QA, QH, QI or simple
